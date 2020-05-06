0461fc4a7ef6ddcc48923d1621d771bda30efa0b
Sejin <skim2257@uwo.ca>
2020-05-06 17:33:15 -0400

    bootstrap-vue pivot

---
+++ script.js
@@ -0,0 +1,24 @@
+new Vue ({
+    el: '#app'
+});
+
+// new Vue ({
+//     el: '#mhmm', 
+//     data: {
+//         state: 'default',
+//         header: 'Im ready to party!',
+//         newItem: '',
+//         items: [
+//             // 'hurdur',
+//             // 'nah', 
+//             // 'bleh'
+//         ]
+//     },
+//     methods: {
+//         saveItem: function() {
+//             this.items.push (this.newItem);
+//             this.newItem='';
+//         }
+//     }
+// });
+
